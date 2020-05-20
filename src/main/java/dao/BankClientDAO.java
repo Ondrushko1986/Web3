59fdd6e2ca4af4ff84e22b5efecf747b2dad8ff8
Ондрюшко <Ondrushko1986@yandex.ru>
2020-05-21 00:08:51 +0300

    1

--- src/main/java/dao/BankClientDAO.java
+++ src/main/java/dao/BankClientDAO.java
@@ -15,7 +15,7 @@ public class BankClientDAO {
     public BankClientDAO(Connection connection) {
         this.connection = connection;
         executor = new Executor(connection);
-    } // +
+    }
 
     public List<BankClient> getAllBankClient() {
         List<BankClient> list = new ArrayList<>();
@@ -30,14 +30,14 @@ public class BankClientDAO {
             e.printStackTrace();
             return null;
         }
-    } // +
+    }
 
     public boolean validateClient(String name, String password) {
         if (getClientByName(name).getPassword().equals(password)) {
             return true;
         }
         return false;
-    } // +
+    }
 
     public boolean updateClientsMoney(String name, String password, Long transactValue) {
         if (validateClient(name, password)) {
@@ -53,7 +53,7 @@ public class BankClientDAO {
             }
         }
         return false;
-    } // +
+    }
 
     public BankClient getClientById(long id) throws SQLException {
         return executor.execQuery("select * from bank_client where id=" + id, result -> {
@@ -61,14 +61,14 @@ public class BankClientDAO {
             return new BankClient(result.getInt(1), result.getString(2),
                     result.getString(3), result.getLong(4));
         });
-    } // +
+    }
 
     public boolean isClientHasSum(String name, Long expectedSum) { // +
         if (getClientByName(name).getMoney() >= Math.abs(expectedSum)) {
             return true;
         }
         return false;
-    } // +
+    }
 
     public long getClientIdByName(String name) {
         try (Statement stmt = connection.createStatement();) {
@@ -83,7 +83,7 @@ public class BankClientDAO {
             e.printStackTrace();
         }
         return 0;
-    } // +
+    }
 
     public BankClient getClientByName(String name) {
         try {
@@ -96,7 +96,7 @@ public class BankClientDAO {
             e.printStackTrace();
             return null;
         }
-    } // +
+    }
 
     public boolean addClient(BankClient client) throws SQLException {
         if (getClientIdByName(client.getName()) <= 0) {
@@ -106,19 +106,19 @@ public class BankClientDAO {
             return true;
         }
         return false;
-    } // +
+    }
 
     public void createTable() throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.execute("create table if not exists bank_client (id bigint auto_increment, name varchar(256), password varchar(256), money bigint, primary key (id))");
         stmt.close();
-    } // +
+    }
 
     public void dropTable() throws SQLException {
         Statement stmt = connection.createStatement();
         stmt.executeUpdate("DROP TABLE IF EXISTS bank_client");
         stmt.close();
-    } // +
+    }
 
     public Connection getConnection() {
         return connection;
